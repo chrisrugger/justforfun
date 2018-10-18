4ac38b918e84b5357b59fc0133b842f84a083359
chrisrugger <chrisrugger@gmail.com>
2018-10-18 00:32:05 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slackfactiva.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slacktiva.settings")
 
 application = get_wsgi_application()

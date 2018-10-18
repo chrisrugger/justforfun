5451f0e8453eb67590f37ae9aaa9a81c3af457f9
chrisrugger <chrisrugger@gmail.com>
2018-10-17 23:52:09 -0400

    Update slackfactiva.py

--- slackfactiva.py
+++ slackfactiva.py
@@ -14,8 +14,85 @@
 # and then run this command line -- you need to get a Factiva access token from http://api.dowjones.com/api/3.0/swagger/ui/index#!/
 # python factivaslack.py <accesstoken>
 # then make sure you're signed into Slack wherever you specify the Slack access token to go to
+##############################
+# THERE WERE UPDATES TO GET THIS TO WORK ON HEROKU
+# set up a heroku account
+# set up a github account
+# connected heroku to github
+#############
+# ADD THIS:
+#####
+# import os
+# factivatoken = os.environ['FACTIVATOKEN']
+# slacktokenvalue = os.environ['SLACKTOKEN']
+# snippetvalue = os.environ['SNIPPETS']
+# urlvalue = os.environ['URLS']
+###
+# create heroku config variables for:
+# FACTIVATOKEN with the Factiva auth token
+# SLACKTOKEN with the slack token
+# SNIPPETS set to on
+# URLS set to on
+############
+# create a file on GitHub called Profile that just says
+# web: python slackfactiva.py
+# this is what makes the Heroku dyno run, which makes the webserver work
+# note -- this code will run once then crash, maybe because all it does is call a Slack API, and has nothing to do afterwards?
+# so I am looking into scheduling a job for it
+# I created a readme in github
+# I created a requirements.txt file, which triggers heroku to know that it is a python thing, and just entered slackclient==1.3.0 -- so that it would import that requirement
+# I changed the Python run time by creating a file called runtime.txt with just this in it: python-2.7.15
+# github is set to automatically deploy to master
+#
+######
+#
+# now I am trying to schedule the job -- followed these steps:
+# https://devcenter.heroku.com/articles/clock-processes-python
+# 
+# added Scheduler to heroku using command line:
+# needed to add my credit card in web interface:
+# https://dashboard.heroku.com/account/billing)
+# 
+# added this to requirements.txt
+# APScheduler==3.0.0
+#
+####
+# created clock.py:
+# from apscheduler.schedulers.blocking import BlockingScheduler
+#
+# sched = BlockingScheduler()
+# 
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=9)
+# def scheduled_job():
+#    print('This job is run every weekday at 9am.')
+#    slackfactiva.py
 
-########################## factivaslack.py
+# sched.start()
+###
+#
+# added this to the Procfile:
+# clock: python clock.py
+# 
+# I am leaving off with heroku crashing, because the page needs something to be running, basically. Perhaps I'll have the page do a redirect after it loads, so it doesn't crash, using something to answer this issue: https://www.reddit.com/r/learnprogramming/comments/2ra2yd/what_makes_a_heroku_app_crash/
+# 
+# also I am starting to add things from here to try to get a webserver running https://devcenter.heroku.com/articles/django-app-configuration
+# down the rabbit hole:
+# https://devcenter.heroku.com/articles/getting-started-with-python
+# https://docs.python-guide.org/starting/install3/osx/
+# https://hathaway.cc/2008/06/how-to-edit-your-path-environment-variables-on-mac/ -- specifically used touch ~/.bash_profile; open ~/.bash_profile
+# and in case you are curious: https://unix.stackexchange.com/questions/111550/what-is-path-on-a-mac-os
+# # Do I have a Python 3 installed?
+# $ python --version
+# Python 3.6.4 # Success!
+# If you still see 2.7 ensure in PATH /usr/local/bin/ takes 
+# precedence over /usr/bin/
+# but I still haven't gotten that to work, I enter
+# python --version
+# and still get 2.7.10
+#########################
+
+
+########################## slackfactiva.py
 import slackclient
 import json
 import urllib
@@ -23,17 +100,15 @@ import time
 from slackclient import SlackClient
 import urllib2
 import sys, getopt
-import os
 ##########################
+# IF YOU RUN ON HEROKU THEN YOU WILL NEED TO:
+# change the arguments below to use os instead, like it says above
+#
 whosealerts = "Here you go:"
-factivatoken = os.environ['FACTIVATOKEN']
-slacktokenvalue = os.environ['SLACKTOKEN']
-snippetvalue = os.environ['SNIPPETS']
-urlvalue = os.environ['URLS']
-#factivatoken = sys.argv[1]
-#slacktokenvalue = sys.argv[2]
-#snippetvalue=sys.argv[3]
-#urlvalue = sys.argv[4]
+factivatoken = sys.argv[1]
+slacktokenvalue = sys.argv[2]
+snippetvalue=sys.argv[3]
+urlvalue = sys.argv[4]
 def sendSlackMyNewHeadlines(accesstokenvar,slacktokenvar,snippetvar,urlvar):
 	accesstoken=accesstokenvar
 	slacktoken=slacktokenvar
@@ -103,6 +178,7 @@ def sendSlackMyNewHeadlines(accesstokenvar,slacktokenvar,snippetvar,urlvar):
 		h = h + '\n:wave:'
 	h = whosealerts + '\n' + h
 	sc.api_call("chat.postMessage", channel="C0UUP7PT5", text= h, username='Gowanus_Bot', icon_url='https://modernmythologies.files.wordpress.com/2014/08/swampthing.png')
+
 sendSlackMyNewHeadlines(factivatoken,slacktokenvalue,snippetvalue,urlvalue)
 
 

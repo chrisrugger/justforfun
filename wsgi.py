96490f593413fd2f3e971abe366f636b9d63e191
chrisrugger <chrisrugger@gmail.com>
2018-10-18 00:15:57 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -13,6 +13,21 @@
 
 #application = get_wsgi_application()
 
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello!"]
+####################
+
+"""
+WSGI config for django_defaults project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_defaults.settings")
+
+application = get_wsgi_application()

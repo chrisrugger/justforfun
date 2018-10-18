b917d43c834c7217d2621381b92cbd875e74449c
chrisrugger <chrisrugger@gmail.com>
2018-10-18 00:20:08 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,33 +1,7 @@
-#"""
-#WSGI config for helloworld project.
-#It exposes the WSGI callable as a module-level variable named ``application``.
-#For more information on this file, see
-#https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-#"""
-
-#import os
-
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
-
-#application = get_wsgi_application()
-
-####################
-
-"""
-WSGI config for django_defaults project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_defaults.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
 
 application = get_wsgi_application()

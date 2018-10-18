66c16b452d8fb20a2a7a37b9f52ab3d5ca2d4b65
chrisrugger <chrisrugger@gmail.com>
2018-10-17 23:35:54 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,18 @@
-"""
-WSGI config for helloworld project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
+#"""
+#WSGI config for helloworld project.
+#It exposes the WSGI callable as a module-level variable named ``application``.
+#For more information on this file, see
+#https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+#"""
 
-import os
+#import os
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["Hello!"]
